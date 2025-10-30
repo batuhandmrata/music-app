6b80aa985a3bf0f8d0c90f6f617e86ccbeaf0cc0
Batuhan Demirata <batus_demirata@hotmail.com>
2025-10-30 17:58:16 +0300

    Ders Sonu

--- app.js
+++ app.js
@@ -14,109 +14,106 @@ const volumeBar = document.querySelector("#volume-bar");
 const player = new MusicPlayer(musicList);
 
 window.addEventListener("load", () => {
-    let music = player.getMusic();
-    displayMusic(music);
-})
+  let music = player.getMusic();
+  displayMusic(music);
+});
 
-function displayMusic(music){
-    title.innerText = music.getName();
-    singer.innerText = music.singer;
-    image.src = "img/" + music.img;
-    audio.src = "mp3/" + music.file ;
+function displayMusic(music) {
+  title.innerText = music.getName();
+  singer.innerText = music.singer;
+  image.src = "img/" + music.img;
+  audio.src = "mp3/" + music.file;
 }
 
 play.addEventListener("click", () => {
-    const isMusicPlay= container.classList.contains("playing");
-    isMusicPlay ? pauseMusic() : playMusic();
-})
+  const isMusicPlay = container.classList.contains("playing");
+  isMusicPlay ? pauseMusic() : playMusic();
+});
 
 prev.addEventListener("click", () => {
-    prevMusic();
-})
+  prevMusic();
+});
 
 next.addEventListener("click", () => {
-    nextMusic();
-})
+  nextMusic();
+});
 
-function prevMusic(){
-    player.prev();
-    let music = player.getMusic();
-    displayMusic(music);
-    playMusic();
+function prevMusic() {
+  player.prev();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
 }
 
-function nextMusic(){
-    player.next();
-    let music = player.getMusic();
-    displayMusic(music);
-    playMusic();
+function nextMusic() {
+  player.next();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
 }
 
-function pauseMusic(){
-    container.classList.remove("playing");
-    play.classList= "fa-solid fa-play";
-    audio.pause();
+function pauseMusic() {
+  container.classList.remove("playing");
+  play.querySelector("i").classList = "fa-solid fa-play";
+  audio.pause();
 }
-function playMusic(){
-    container.classList.add("playing");
-    play.classList= "fa-solid fa-pause";
-    audio.play();
+function playMusic() {
+  container.classList.add("playing");
+  play.querySelector("i").classList = "fa-solid fa-pause";
+  audio.play();
 }
 
 const calculateTime = (toplamSaniye) => {
-    const dakika = Math.floor(toplamSaniye / 60);
-    const saniye = Math.floor(toplamSaniye % 60);
-    const guncellenenSaniye = saniye < 10 ? `0${saniye}` : `${saniye}`;
-    const sonuc = `${dakika}:${guncellenenSaniye}`;
-    return sonuc;
-}
+  const dakika = Math.floor(toplamSaniye / 60);
+  const saniye = Math.floor(toplamSaniye % 60);
+  const guncellenenSaniye = saniye < 10 ? `0${saniye}` : `${saniye}`;
+  const sonuc = `${dakika}:${guncellenenSaniye}`;
+  return sonuc;
+};
 
 audio.addEventListener("loadedmetadata", () => {
-    duration.textContent = calculateTime(audio.duration);
-    progressBar.max=Math.floor(audio.duration);
-    progressBar.step=1;
-})
+  duration.textContent = calculateTime(audio.duration);
+  progressBar.max = Math.floor(audio.duration);
+  progressBar.step = 1;
+});
 
 audio.addEventListener("timeupdate", () => {
-    progressBar.value = Math.floor(audio.currentTime);
-    currentTime.textContent = calculateTime(progressBar.value);
-})
+  progressBar.value = Math.floor(audio.currentTime);
+  currentTime.textContent = calculateTime(progressBar.value);
+});
 
 progressBar.addEventListener("input", () => {
-    currentTime.textContent = calculateTime(progressBar.value);
-    audio.currentTime = progressBar.value;
-})
+  currentTime.textContent = calculateTime(progressBar.value);
+  audio.currentTime = progressBar.value;
+});
 
 let sesDurumu = "sesli";
 
 volumeBar.addEventListener("input", (e) => {
-    const value = e.target.value;
-    audio.volume = value / 100 ;
-    if(value ==0){
-        audio.muted = true;
-        sesDurumu = "sessiz";
-        volume.classList ="fa-solid fa-volume-xmark";
-    }else{
-        audio.muted=false ;
-        sesDurumu = "sesli";
-        volume.classList="fa-solid fa-volume-high";
-    }
-})
+  const value = e.target.value;
+  audio.volume = value / 100;
+  if (value == 0) {
+    audio.muted = true;
+    sesDurumu = "sessiz";
+    volume.classList = "fa-solid fa-volume-xmark";
+  } else {
+    audio.muted = false;
+    sesDurumu = "sesli";
+    volume.classList = "fa-solid fa-volume-high";
+  }
+});
 let lastVolume;
 volume.addEventListener("click", () => {
-    if(sesDurumu==="sesli"){
-        lastVolume = volumeBar.value;
-        audio.muted = true;
-        sesDurumu = "sessiz";
-        volume.classList = "fa-solid fa-volume-xmark";
-        volumeBar.value=0;
-    }else{
-        audio.muted = false;
-        sesDurumu = "sesli";
-        volume.classList = "fa-solid fa-volume-high";
-        volumeBar.value=lastVolume;
-    }
-})
-
-
-
+  if (sesDurumu === "sesli") {
+    lastVolume = volumeBar.value;
+    audio.muted = true;
+    sesDurumu = "sessiz";
+    volume.classList = "fa-solid fa-volume-xmark";
+    volumeBar.value = 0;
+  } else {
+    audio.muted = false;
+    sesDurumu = "sesli";
+    volume.classList = "fa-solid fa-volume-high";
+    volumeBar.value = lastVolume;
+  }
+});
